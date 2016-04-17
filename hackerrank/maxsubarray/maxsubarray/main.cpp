839fead2d6895d1d934f808de89d7b82f7d0ee9a
Ashwin <2ashwin@gmail.com>
2016-04-17 10:25:34 +0530

    adding initial version

---
+++ hackerrank/maxsubarray/maxsubarray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  maxsubarray
+//
+//  Created by Ashwin Kumar Surabhi on 17/04/16.
+//  Copyright © 2016 ashwinco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
