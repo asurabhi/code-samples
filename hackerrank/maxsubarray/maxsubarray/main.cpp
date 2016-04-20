b1e3428091780c38b1e91b808ca1ae6f49cc5276
Ashwin <2ashwin@gmail.com>
2016-04-20 21:40:05 +0530

    working version

--- hackerrank/maxsubarray/maxsubarray/main.cpp
+++ hackerrank/maxsubarray/maxsubarray/main.cpp
@@ -10,6 +10,50 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    /*
+    1≤T≤101≤T≤10
+    1≤N≤1051≤N≤105
+    −104≤ai≤104
+    */
+    int T,N;
+    long long arr[1000*100];
+    long long sum, largest;
+    scanf("%d", &T);
+    for (int i=0;i<T;i++) {
+        scanf("%d",&N);
+        for (int j=0;j<N;j++) {
+            scanf("%lld", &arr[j]);
+        }
+        largest = arr[0];
+        if (arr[0]>0) {
+            sum = arr[0];
+        } else {
+            sum = 0;
+        }
+        for (int j=0;j<N;j++) {
+            if (arr[j]>largest) {
+                largest = arr[j];
+            }
+        }
+        for (int j=1;j<N;j++) {
+            if (arr[j]>0) {
+                sum += arr[j];
+            }
+            if (arr[j-1]>0) {
+                arr[j] += arr[j-1];
+            }
+        }
+        
+        if (sum == 0) {
+            sum = largest;
+        }
+        
+        for (int j=0;j<N;j++) {
+            if (arr[j]>largest) {
+                largest = arr[j];
+            }
+        }
+        printf("%lld %lld\n", largest, sum);
+    }
     return 0;
 }
